83e9eba7b674e46fff92d38a54587e143eab2069
huangpw <1042850644@qq.com>
2023-04-19 02:24:46 +0800

    deploy

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -5,7 +5,7 @@ const footer = require('./cofig/footer');
 const sidebar = require('./cofig/sidebar');
 
 export default defineConfig({
-  base: './react-press-docs/', // 需要在路径前面配置./ 不然样式失效
+  base: '/react-press-docs/', // 需要在路径前面配置./ 不然样式失效
   title: 'React Docs',
   description: 'React Docs',
   lang: 'zh-CN', // default language（中文zh-CN）（英文en-US）
